e7389adf0fadbf3e54fcaf2f8a745d7f9d3c25b5
Tomas Ostos <t.ostos@uniandes.edu.co>
2026-03-14 20:52:18 -0500

    PROFE UN 5.55555

--- i18n-preparcial/app/[lang]/components/Footer.tsx
+++ i18n-preparcial/app/[lang]/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer(){
 
 return(
 
-    <footer className="bg-[#BBCCBB] flex justify-between items-center px-48 py-2 text-sm font-semibold">
+    <footer className="bg-[#BBCCBB] flex justify-between items-center px-48 py-2 text-sm font-extrabold">
 
 <p>© 2026 Harry Potter App. Todos los derechos reservados.</p>
 <p>Desarrollado para: ISIS3710</p>
